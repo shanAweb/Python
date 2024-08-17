f1589f487051399dfd814603c3c4a68b259baf24
shanAweb <shanasif2002@gmail.com>
2024-08-17 16:36:44 +0500

    Tried math funtions using math module

---
+++ mathFunctions.py
@@ -0,0 +1,7 @@
+import math
+x=2.9
+print(math.ceil(x))
+print(math.floor(x))
+print(math.factorial(math.ceil(x))) #got the ceil value integer using math module and got factorial
+print(math.isfinite(x))
+#A lot more in math module to implement
