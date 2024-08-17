ca360fb3cf06378b36388baed90cf43d08e6d2f2
shanAweb <shanasif2002@gmail.com>
2024-08-17 12:50:08 +0500

    Added first python exercise and python input by extending exercise

---
+++ exercise1.py
@@ -0,0 +1,4 @@
+patient_name="Jonn D Smith"
+age = 20
+is_new = True
+print("Patient Named " + patient_name + " age " + str(age) + " " + str(is_new) + " patient")
---
+++ input.py
@@ -0,0 +1,5 @@
+#Getting input in python
+name = input("What is your name? ")
+age = input ("What is your age? ")
+is_new = input ("Are you a new patient or old? ")
+print("Patient " + name + " is " + age + " years old " + " and is " + is_new + " patient" )
