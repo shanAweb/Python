f756c9a542892e0271bf51f250cf8d99cf546000
shanAweb <shanasif2002@gmail.com>
2024-08-17 13:42:29 +0500

    Cloned a string to understand character access from a string

---
+++ cloneString.py
@@ -0,0 +1,5 @@
+#Cloning of a string is being used to understand how can we access string elements
+course="Python's course for Beginners"
+course_title=course[:] #This will access all elements i.e [start:end]
+#We can access specific elements in the same way
+print(course_title)
