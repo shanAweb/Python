9abca2556741f971fe92c24b83f3d4a091fde531
shanAweb <shanasif2002@gmail.com>
2024-08-17 15:08:44 +0500

    used methods to understand find, replace and exist properites

---
+++ find-replace-exist.py
@@ -0,0 +1,7 @@
+goal="Become a Python developer"
+#Find - case-sensitive
+print(goal.find('Become')) #starting index is returned
+#Replace - case-sensitive
+print(goal.replace("developer","expert"))
+#exist - case-sensitive
+print("Python" in goal) #boolean value is returned
