95ba06cc9e5f0de7a0b288deadf60aa96bc25dc6
shanAweb <shanasif2002@gmail.com>
2024-08-17 13:31:49 +0500

    Different representations of string and Multi-line string

---
+++ strings.py
@@ -0,0 +1,10 @@
+subject='Welcome message to new "Staff"'
+sender=""" FROM: 
+Office's HR office """
+message = """
+Hi John!
+Welcome to the office!. Get the best of yours under this work friendly environment. 
+Best Regards,
+The HR Team
+"""
+print(subject + sender + message)
