7edbafb23546d32fec78eb7039ee292771096852
shanAweb <shanasif2002@gmail.com>
2024-08-18 13:45:20 +0500

    Understand while loop by making a triangle

---
+++ triangle.py
@@ -0,0 +1,9 @@
+i=1
+while i<=5:
+    print("*" * i)
+    i+=1
+#To print reverse triangles (make an arrow head), below lines needs to be uncommented
+# i=5
+# while i>0:
+    # print("*" * i)
+    # i-=1
