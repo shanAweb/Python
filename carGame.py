c67609961c534ba36e49a8419aa9be73a190ff41
shanAweb <shanasif2002@gmail.com>
2024-08-18 15:32:48 +0500

    Developed a car game engine for real time scenerio experience of python if else and while

---
+++ carGame.py
@@ -0,0 +1,28 @@
+command=""
+started = False
+print('"help" to get help to start the game')
+while True:
+    command=input("> ").lower()
+    if command=="start":
+        if started:
+            print("Car is already started!")
+        else:
+            started=True
+            print("Car Started...")
+    elif command=="stop":
+        if not started:
+            print("Car is already stopped!")
+        else:
+            started=False
+            print("Car Stopped.")
+    elif command=="help":
+        print("""
+start - to start the car
+stop - to stop the car
+quit - to quit the game
+        """)
+    elif command=="quit":
+        print("Game Quited..")
+        break
+    else:
+        print("Sorry! I don't understand this")
