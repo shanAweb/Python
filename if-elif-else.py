4a0b301592cee9a6394cdd2f887235d844b3a5ef
shanAweb <shanasif2002@gmail.com>
2024-08-18 11:19:42 +0500

    Understand the functionality of if - elif - else statements

---
+++ if-elif-else.py
@@ -0,0 +1,12 @@
+#if - elif - else executes specific piece of code with subject to some certain conditions
+is_hot = False
+is_cold = False
+if is_hot:
+    print("Its a hot day")
+    print("Drink plenty of water")
+elif is_cold:
+    print("its a cold day")
+    print("Wear warm cloths")
+else:
+    print("Its a lovely day")
+print("Enjoy your day!")
