8fa5d376d66eb15e8f7f0789e1056d1e00f7ad9d
Shan Asif <shanasif2002@gmail.com>
2024-10-16 13:45:03 +0500

    Functions in python

---
+++ functions.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a+b
+
+
+sum_of_numbers = add(2, 3)
+print(sum_of_numbers)
+
+sum_of_numbers = add(5, 5)
+print(sum_of_numbers)
+
+#above is the methods, how can we initialize or call a function
