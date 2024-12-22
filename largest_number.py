93adc67b68fc3ee9d69d28e3fff6e69b54a62115
shanAweb <shanasif2002@gmail.com>
2024-12-22 15:01:55 +0500

    Understood the concept of modules and did a module practice

---
+++ largest_number.py
@@ -0,0 +1,10 @@
+def find_max(list):
+    largest_number=list[0]
+    for item in list:
+        if item > largest_number:
+            largest_number=item
+    return largest_number
+
+
+
+
---
+++ module.py
@@ -0,0 +1,4 @@
+from largest_number import find_max
+numbers=[150,200,300,400,500]
+largest_number=find_max(numbers)
+print(largest_number)
